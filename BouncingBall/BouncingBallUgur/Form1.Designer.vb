52b8395daae0747bf5e2ae723b6de4b42dc5a2e9
Ugur Yilmaz <uyilmaz1@binghamton.edu>
2018-01-03 13:26:09 -0800

    Bouncing Ball

---
+++ BouncingBall/BouncingBallUgur/Form1.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TrackBar2 = New System.Windows.Forms.TrackBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(458, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 52)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(458, 75)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 52)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(458, 147)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 52)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Speed++"
+        '
+        '
+        'TrackBar2
+        '
+        Me.TrackBar2.Location = New System.Drawing.Point(458, 218)
+        Me.TrackBar2.Maximum = 360
+        Me.TrackBar2.Name = "TrackBar2"
+        Me.TrackBar2.Size = New System.Drawing.Size(104, 45)
+        Me.TrackBar2.TabIndex = 3
+        Me.TrackBar2.TickFrequency = 30
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(455, 250)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Change Direction "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(540, 163)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 13)
+        Me.Label3.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(511, 218)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 13)
+        Me.Label4.TabIndex = 8
+        '
+
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(584, 361)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TrackBar2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button3)
+        Me.Name = "Form1"
+        Me.Text = "Ugur Yilmaz"
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TrackBar2 As TrackBar
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button3 As Button
+End Class
---
+++ BouncingBall/BouncingBallUgur/Form1.vb
@@ -0,0 +1,86 @@
+﻿Imports Microsoft.VisualBasic.PowerPacks
+
+Public Class Form1
+    Dim show_speed As Integer
+    Dim move_right As Boolean
+    Dim move_top As Boolean
+    Dim speed As Integer
+    Dim containerShape As New ShapeContainer
+    Dim x_speed As Integer
+    Dim y_speed As Integer
+    Dim ovalShape As New OvalShape
+    Dim angle As Double
+
+
+    Private Function DrawBall()
+        containerShape.Parent = Me
+        ovalShape.Parent = containerShape
+        ovalShape.FillStyle = FillStyle.Solid
+        ovalShape.FillColor = Color.Blue
+        ovalShape.Size = New System.Drawing.Size(24, 24)
+        ovalShape.Location = New System.Drawing.Point((ClientSize.Width - 24) \ 2, (ClientSize.Height - 24) \ 2)
+    End Function
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DrawBall()
+        Timer1.Stop()
+        Label3.Text = "8"
+        TrackBar2.Value = 45
+        Label4.Text = "45"
+        speed = Label3.Text
+        x_speed = Math.Cos(Math.PI * angle / 180.0) * ((2 * speed) ^ (0.5))
+        y_speed = Math.Sin(Math.PI * angle / 180.0) * ((2 * speed) ^ (0.5))
+    End Sub
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If (ovalShape.Top - y_speed) <= 0 Then
+            move_top = False
+        End If
+        If (ovalShape.Left - x_speed) <= 0 Then
+            move_right = True
+        End If
+        If (ovalShape.Bottom + y_speed) >= Me.ClientRectangle.Bottom Then
+            move_top = True
+        End If
+        If move_right = False Then
+            ovalShape.Left -= x_speed
+        Else
+            ovalShape.Left += x_speed
+        End If
+        If (ovalShape.Right + x_speed) >= Me.ClientRectangle.Right Then
+            move_right = False
+        End If
+        If move_top = False Then
+            ovalShape.Top += y_speed
+        Else
+            ovalShape.Top -= y_speed
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Start()
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Button3_Click_1(sender As Object, e As EventArgs) Handles Button3.Click
+        show_speed = Convert.ToInt32(Label3.Text)
+        show_speed = show_speed + 20
+        Label3.Text = Convert.ToString(show_speed)
+        speed = Label3.Text
+        x_speed = Math.Cos(Math.PI * angle / 180.0) * ((2 * Convert.ToInt32(speed)) ^ (0.5))
+        y_speed = Math.Sin(Math.PI * angle / 180.0) * ((2 * Convert.ToInt32(speed)) ^ (0.5))
+    End Sub
+
+    Private Sub TrackBar2_ValueChanged(sender As Object, e As EventArgs) Handles TrackBar2.ValueChanged
+        angle = TrackBar2.Value
+        Label4.Text = TrackBar2.Value
+        x_speed = Math.Cos(Math.PI * angle / 180.0) * ((2 * speed) ^ (0.5))
+        y_speed = Math.Sin(Math.PI * angle / 180.0) * ((2 * speed) ^ (0.5))
+    End Sub
+
+End Class
